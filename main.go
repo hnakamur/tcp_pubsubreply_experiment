16768d6334729dedab20dbd7770725e377d2f884
Hiroaki Nakamura <hnakamur@gmail.com>
2016-05-22 08:52:06 +0000

    Clean up debug logs

--- main.go
+++ main.go
@@ -107,37 +107,25 @@ func (s *server) registerWorker(workerID string) workerChannel {
 		jobResultC: make(chan *JobResult),
 	}
 	s.workerChannels[workerID] = c
-	fmt.Printf("server added channel for workerID: %s\n", workerID)
-	for workerID, _ := range s.workerChannels {
-		fmt.Printf("workerID in s.workerChannels: %s\n", workerID)
-	}
 	return c
 }
 
 func (s *server) dispatchJob() {
 	for {
-		fmt.Printf("server waiting job in dispatchJob\n")
 		job := <-s.jobC
-		fmt.Printf("server received job in dispatchJob: %v\n", job)
 		s.mu.Lock()
-		for workerID, workerChannel := range s.workerChannels {
-			fmt.Printf("server sending Job %v to worker %s channel\n", job, workerID)
+		for _, workerChannel := range s.workerChannels {
 			workerChannel.jobC <- job
-			fmt.Printf("server sent Job %v to worker %s channel\n", job, workerID)
 		}
 		jobResults := &JobResults{
 			Results: make([]*JobResult, 0, len(s.workerChannels)),
 		}
-		for workerID, workerChannel := range s.workerChannels {
-			fmt.Printf("server waiting JobResult from worker %s channel\n", workerID)
+		for _, workerChannel := range s.workerChannels {
 			jobResult := <-workerChannel.jobResultC
-			fmt.Printf("server received JobResult %v from worker %s channel\n", jobResult, workerID)
 			jobResults.Results = append(jobResults.Results, jobResult)
 		}
 		s.mu.Unlock()
-		fmt.Printf("server sending jobResults to jobResultsC\n")
 		s.jobResultsC <- jobResults
-		fmt.Printf("server sent jobResults to jobResultsC\n")
 	}
 }
 
@@ -150,7 +138,6 @@ func (s *server) handleConnection(conn net.Conn) {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("server received message type: %s\n", msgType)
 		switch msgType {
 		case MessageType_JobMsg:
 			job, err := rw.ReadJob()
@@ -159,10 +146,8 @@ func (s *server) handleConnection(conn net.Conn) {
 			}
 			fmt.Printf("server received Job %v\n", job)
 			s.jobC <- job
-			fmt.Printf("server sent Job %v to jobC\n", job)
 
 			jobResults := <-s.jobResultsC
-			fmt.Printf("server received JobResults %v from jobResultsC\n", jobResults)
 			err = rw.WriteTypeAndJobResults(jobResults)
 			if err != nil {
 				log.Fatal(err)
@@ -174,23 +159,20 @@ func (s *server) handleConnection(conn net.Conn) {
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("server received RegisterWorker message: %v\n", registerWorker)
 			workerChannel := s.registerWorker(registerWorker.WorkerID)
 			fmt.Printf("server registered worker: %v\n", registerWorker.WorkerID)
 			for {
-				fmt.Printf("server waiting job from dispatchC for worker %s\n", registerWorker.WorkerID)
 				job := <-workerChannel.jobC
-				fmt.Printf("server received job from dispatchC %v. sending it to worker %s\n", job, registerWorker.WorkerID)
 				err = rw.WriteTypeAndJob(job)
 				if err != nil {
 					log.Fatal(err)
 				}
+				fmt.Printf("server sent Job %v to worker %v\n", job, registerWorker.WorkerID)
 
 				msgType, err := rw.ReadMessageType()
 				if err != nil {
 					log.Fatal(err)
 				}
-				fmt.Printf("server received message type %s from worker %s\n", msgType, registerWorker.WorkerID)
 				switch msgType {
 				case MessageType_JobResultMsg:
 					jobResult, err := rw.ReadJobResult()
@@ -199,7 +181,6 @@ func (s *server) handleConnection(conn net.Conn) {
 					}
 					fmt.Printf("server received JobResult %v from worker %s\n", jobResult, registerWorker.WorkerID)
 					workerChannel.jobResultC <- jobResult
-					fmt.Printf("server sent JobResult %v from worker %s to jobResultC\n", jobResult, registerWorker.WorkerID)
 				}
 			}
 
@@ -231,19 +212,17 @@ func workerCommand(args []string) {
 	fmt.Printf("worker registered myself: %s\n", workerID)
 
 	for {
-		fmt.Printf("worker %s waiting message from server.\n", workerID)
 		msgType, err := rw.ReadMessageType()
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("worker %s received message type from server. type: %s\n", workerID, msgType)
 		switch msgType {
 		case MessageType_JobMsg:
 			job, err := rw.ReadJob()
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("worker %s received Job message: %v\n", workerID, job)
+			fmt.Printf("worker %s received Job %v\n", workerID, job)
 
 			jobResult := &JobResult{
 				WorkerID: workerID,
@@ -259,6 +238,7 @@ func workerCommand(args []string) {
 			if err != nil {
 				log.Fatal(err)
 			}
+			fmt.Printf("worker %s sent JobResult %v\n", workerID, jobResult)
 		}
 	}
 }
@@ -285,13 +265,12 @@ func requestCommand(args []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("client sent Job %v to server.\n", job)
 
-	fmt.Printf("client waiting jobResults from server.\n")
 	msgType, err := rw.ReadMessageType()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("client received message type from server. type: %s\n", msgType)
 	switch msgType {
 	case MessageType_JobResultsMsg:
 		jobResults, err := rw.ReadJobResults()
